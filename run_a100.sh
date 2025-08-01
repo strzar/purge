32c69e3ae07a18a13f3247d0d9fb441f4bf46c9a
strzar <12795388+strzar@users.noreply.github.com>
2025-08-01 12:12:32 +0300

    Update run_a100.sh

--- run_a100.sh
+++ run_a100.sh
@@ -20,4 +20,4 @@
 #SBATCH --cpus-per-task=4   ## cores (default 2)
 
 #Run your program / code here
-python traverse_first_21_and_count_json_fields.py --root /home/stratis/unlearning/PURGE --tokenizer-json /mnt/raid5/stratis/models/main/Phi-3-mini-4k-instruct-1_Stephen_King/checkpoint-500/tokenizer.json  --limit 21 --first-n 100
+python grpo.py
